03f9b893cccb15e6d3cae63ffe48a3fd5b4450fc
kapil <kapil.kumar@primussoft.com>
2020-04-27 08:53:20 -0400

    first repo

---
+++ src/main/java/com/demo/ExcelReader/ExcelReader.java
@@ -0,0 +1,7 @@
+package com.demo.ExcelReader;
+
+public class ExcelReader {
+	
+	
+
+}
---
+++ src/test/java/com/demo/TestScripts/Verify_The_Registrartion_Page.java
@@ -0,0 +1,17 @@
+package com.demo.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class Verify_The_Registrartion_Page {
+	
+	@Test()
+	public void testmethod() {
+		System.out.println("test scripts");
+	}
+	
+	@Test()
+	public void testmethod1() {
+		System.out.println("test scripts1");
+	}
+
+}
