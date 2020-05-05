e02abff00788a2b289ce595f0773b928277e01c7
kapil <kapil.kumar@primussoft.com>
2020-05-05 06:52:49 -0400

    changes in the test scripts

--- src/test/java/com/demo/TestScripts/Verify_The_Registrartion_Page.java
+++ src/test/java/com/demo/TestScripts/Verify_The_Registrartion_Page.java
@@ -1,5 +1,6 @@
 package com.demo.TestScripts;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.guruweb.testBase.TestBase;
@@ -8,11 +9,31 @@ import com.relevantcodes.extentreports.LogStatus;
 public class Verify_The_Registrartion_Page extends TestBase {
 	
 	@Test()
-	public void testmethod() {
+	public void LoginToTheApplication() {
 		
-		System.out.println("Application launched ");
 		test.log(LogStatus.PASS, "Guru Application launched succesfully launched");
+		
+		
+			try {
+				driver.findElement(By.name("firstName")).sendKeys("Kapil");
+				test.log(LogStatus.PASS, "FirstName entered");
+				driver.findElement(By.name("lastName")).sendKeys("Singh");
+				test.log(LogStatus.PASS, "lastname entered");
+				driver.findElement(By.name("phone")).sendKeys("857515427");
+				test.log(LogStatus.PASS, "phone number entered");
+				driver.findElement(By.name("submit")).click();
+				test.log(LogStatus.PASS, "clicked on the submit button");
+			} catch (Exception e) {
+				
+				test.log(LogStatus.FAIL, "unable to click on the submit button");
+			}
+	
+		
+		
+		
+			
+		}
 	}
 	
 	
-}
+
